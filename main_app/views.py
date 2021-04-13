781d74c499abf4f3392663d710e14c2d4e120a92
dshindel <dannyshindel@gmail.com>
2021-04-13 11:45:56 -0700

    API initial

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Craft
 from django.views.generic import ListView
+import requests
 
 # Create your views here.
 
@@ -8,4 +9,17 @@ class Index(ListView):
     model = Craft
     fields = ['name']
     
+def user_index(request):
+    crafts = Craft.objects.all()
+    return render(request, 'spacecrafts/index.html', { 'crafts': crafts })
+
+def search(request):
+    response = requests.get('https://swapi.dev/api/starships')
+    results = response.json()
+    starships = results['results']
+    return render(request, 'spacecrafts/search.html', { 'starships': starships })
+
+def form(request):
+    test = request.POST
+    return render(request, 'spacecrafts/form.html', { 'test': test })
 
