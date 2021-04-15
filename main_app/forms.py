fa1562a46d0f157b8168affd0063c62049cdf88b
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-04-14 17:03:36 -0700

    url form

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,6 +4,7 @@ from .models import Craft
 class CraftForm(ModelForm):
     class Meta:
         model = Craft
-        fields = "__all__" 
+        # fields = ['cargo_capacity', 'consumables', 'cost_in_credits', 'crew', 'length', 'manufacturer', 'max_atmosphering_speed', 'model', 'name', 'passengers', 'hyperdrive_rating', 'vehicle_class', 'starship_class', 'MGLT', 'sell_price', 'condition', 'description', 'mileage'] 
+        fields= '__all__'
 
         
