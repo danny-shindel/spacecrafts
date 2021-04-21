a1986d5a82faa06393ee63d3aff737337baad538
dshindel <dannyshindel@gmail.com>
2021-04-21 10:29:26 -0700

    MVP

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Craft, Favorite, Photo, Badge
+from .models import Craft, Favorite, Photo, Badge, Image
 
 # Register your models here.
 admin.site.register(Craft)
 admin.site.register(Favorite)
 admin.site.register(Photo)
 admin.site.register(Badge)
+admin.site.register(Image)
